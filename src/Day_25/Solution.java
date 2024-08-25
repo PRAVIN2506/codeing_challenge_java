c99425c3e30717c92b12d8030d7894f8ce3729e2
PRAVIN B <peterpravin2506@gmail.com>
2024-08-26 00:11:11 +0530

    day_25 completed

---
+++ src/Day_25/Solution.java
@@ -0,0 +1,42 @@
+package Day_25;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(s.postorderTraversal(root));
+
+    }
+
+    public void traversal(TreeNode root, List<Integer> res){
+        if (root != null){
+            traversal(root.left, res);
+            traversal(root.right, res);
+            res.add(root.val);
+        }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        traversal(root, res);
+        return res;
+    }
+}
