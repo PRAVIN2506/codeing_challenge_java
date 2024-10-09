80ad71aa6ac4103052caa9faffb5fef0ca474fd5
PRAVIN B <peterpravin2506@gmail.com>
2024-10-09 06:40:11 +0530

    Day_70 completed

---
+++ src/Day_70/Solution.java
@@ -0,0 +1,22 @@
+package Day_70;
+
+import java.util.Stack;
+
+public class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char x : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == '(' && x == ')') {
+                stack.pop();
+            } else {
+                stack.push(x);
+            }
+        }
+        return stack.size();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.minAddToMakeValid("())"));
+    }
+}
