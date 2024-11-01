8d8dfa2319df0ad3ae020a682c2d90fe7fba662a
PRAVIN B <peterpravin2506@gmail.com>
2024-11-01 16:17:11 +0530

    Day_90 completed

---
+++ src/Day_90/Solution.java
@@ -0,0 +1,27 @@
+package Day_90;
+
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder ans = new StringBuilder();
+        ans.append(s.charAt(0));
+        int n = s.length(), cnt = 1;
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == ans.charAt(ans.length() - 1)) {
+                cnt++;
+                if (cnt < 3) {
+                    ans.append(s.charAt(i));
+                }
+            } else {
+                cnt = 1;
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        String s1="praveen";
+        System.out.println(s.makeFancyString(s1));
+    }
+}
